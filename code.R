d1fd71aaa8d98aa3abc54b20767e638ecf152b99
sophieehill <43281937+sophieehill@users.noreply.github.com>
2021-05-22 19:04:08 -0400

    .

--- code.R
+++ code.R
@@ -11,27 +11,22 @@ connections <- read_csv("connections.csv")
 # sort people alphabetically so that the selection list is easier to follow
 people <- people[order(people$id),]
 
-# calculate degree cenrality in order to scale nodes
-graph <- igraph::graph.data.frame(connections, directed = F)
-degree_value <- degree(graph, mode = "in")
-# scaling factor 
-people$icon.size <- degree_value[match(people$id, names(degree_value))] + 25
-people$icon.size <- as.integer(people$icon.size)
-
-
 # add attributes
 people <- people %>% mutate(shape = shape, 
-                            image = paste0("/photos/", gsub(" ", "_", id), ".png"),
-                            label = id)
+                            image = paste0("https://raw.githubusercontent.com/sophieehill/untold-murder/main/photos/", gsub(" ", "_", id), ".png"),
+                            label = id,
+                            size = case_when(id=="Daniel Morgan" ~ 80,
+                                             id=="Jonathan Rees" ~ 60,
+                                             id=="Sid Fillery" ~ 60,
+                                             TRUE ~ 40))
 people$title <- paste0("<p>", people$desc,"</p>")
-people$font.size <- people$icon.size/2
 
 # connections$label <- connections$type
 connections$title <- paste0("<p>", connections$detail, "</p>")
 connections$color.highlight <- "yellow"
 connections$color.hover <- "yellow"
 # make sure "value" is saved as an integer variable
-connections$value <- 20
+connections$value <- 10
 
 
 # save datasets to call in Shiny
--- save_to_html.R
+++ save_to_html.R
@@ -1,7 +1,7 @@
 # library("visNetwork")
-# 
-# load("people.RData")
-# load("connections.RData")
+
+load("people.RData")
+load("connections.RData")
 
 
 visNetwork(people, connections, width = "1000px", height = "600px", 
@@ -9,8 +9,8 @@ visNetwork(people, connections, width = "1000px", height = "600px",
                      style='font-family:Source Sans Pro, Helvetica, sans-serif;font-weight:bold;font-size:22px;text-align:left;'),
            submain=list(text="The Daniel Morgan murder case \n",
                      style='font-family:Source Sans Pro, Helvetica, sans-serif;font-style:italic;font-size:14px;text-align:left;')) %>%
-  visEdges(scaling=list(min=8, max=40), smooth=FALSE) %>%
-  visNodes(scaling=list(min=100, max=100)) %>%
+  visEdges(smooth=TRUE) %>%
+  visNodes(shapeProperties = list(useBorderWithImage = F)) %>%
   visOptions(highlightNearest = list(enabled = T, degree = 2, 
                                      hover = T, hideColor = 'rgba(200,200,200,0.5)',
                                      labelOnly=FALSE),
@@ -18,7 +18,7 @@ visNetwork(people, connections, width = "1000px", height = "600px",
   visInteraction(hover=TRUE, zoomView = TRUE,
                  navigationButtons = TRUE,
                  tooltipStyle = 'position: fixed;visibility:hidden;padding: 5px;
-                font-family: sans-serif;font-size:12px;
+                font-family: sans-serif;font-size:16px;
                 font-color:#000000;background-color: #e3fafa;
                 -moz-border-radius: 3px;-webkit-border-radius: 3px;border-radius: 3px;
                  border: 0px solid #808074;box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
@@ -26,8 +26,8 @@ visNetwork(people, connections, width = "1000px", height = "600px",
   visPhysics(solver = "forceAtlas2Based", 
              maxVelocity = 1000,
              minVelocity = 5,
-             forceAtlas2Based = list(gravitationalConstant = -150),
-             stabilization = FALSE) %>%
+             forceAtlas2Based = list(gravitationalConstant = -50),
+             stabilization = TRUE) %>%
   addFontAwesome() %>%
   visLayout(randomSeed = 02143) %>%
   visSave(file = "untold-murder.html", selfcontained = TRUE)
